d82c163e79f82f8dbc5a58ef0acc2ff992f8fc29
ShahdAllemie0 <69586588+ShahdAllemie0@users.noreply.github.com>
2020-08-16 16:45:33 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,22 +1,32 @@
 
 def main():
-      num1 = input(" Enter the first number :  ")
-      n1 = int(number1)
-      num2 = input(" Enter the second number : ")
-      n2 = int(number2)
-      op = input(" Choose the operation (+, -, /, ) : ")
-      result = 0
-      if op == "+":
-        print(result  =  n1+n2)
-      elif op == "-":
-        print(result  = n1-n2)
-      elif op == "/":
-        print(result  = n1/n2)
-      elif op == "*":
-        print(result  = n1*n2)
-      else:
-        print("the numbers were invalid")
-
+  num11=input("Enter the first number: ")
+  num1=int(num11)
+  num22=input("Enter the second number: ")
+  num2=int(num22)
+  op=input("Choose the operation (+, -, /, *): ")
+ 
+#   cal(num1,num2,op)
+#   def cal(num1,num2,op):
+  result=0
+ 
+  if num1.isnumeric() and num1.isnumeric():
+	 if op == "+":
+		 result=num1+nume2
+		 print("The answer is 24 "+result)
+	 elif op=='-':
+		 result=num1-nume2
+		 print("The answer is 24 "+result)
+	 elif op=='/':
+		 result=num1/nume2
+		 print("The answer is 24 "+result)
+	 elif op=='*':
+		 result=num1*nume2
+		 print("The answer is 24 "+result)
+	 else:
+		print("operation is not valid")
+  else:		
+	  print("the numbers were invalid")	 	
 
 
 if __name__ == '__main__':

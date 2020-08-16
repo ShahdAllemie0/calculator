8e2900575cae75ab5007dab569e0ca7b94648917
ShahdAllemie0 <69586588+ShahdAllemie0@users.noreply.github.com>
2020-08-16 15:38:38 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,31 @@
 
 def main():
 	#write your code here
+  num1=input("Enter the first number: ")
+  num2=input("Enter the second number: ")
+  op=input("Choose the operation (+, -, /, *): ")
+
+ 
+  if num1.isnumeric() and num1.isnumeric():
+	 if op=='+':
+		 result=num1+nume2
+		 print("The answer is 24 "+result)
+	 elif op=='-':
+		 result=num1-nume2
+		 print("The answer is 24 "+result)
+	 elif op=='/':
+		 result=num1/nume2
+		 print("The answer is 24 "+result)
+	 elif op=='*':
+		 result=num1*nume2
+		 print("The answer is 24 "+result)
+	 else:
+		print("operation is not valid")
+  else:		
+	  print("the numbers were invalid")	 		     		 		 
+ 		
+	
+
 	pass
 	
 

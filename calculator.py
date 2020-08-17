ab765023d94a1a6ec0439a987a6b61ec12f18873
ShahdAllemie0 <69586588+ShahdAllemie0@users.noreply.github.com>
2020-08-17 09:49:56 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,33 +1,34 @@
 
 def main():
-  
-num1=input("Enter the first number:")
-num2=input("Enter the second number: ")
-op=input("Choose the operation (+, -, /, *): ")
+     
+    num1=input("Enter the first number:")
+    num2=input("Enter the second number: ")
+    op=input("Choose the operation (+, -, /, *): ")
 
 
   
   
-def calc(num1,num2,op):
-   if (num1.isnumeric() and num2.isnumeric())==True:
-	   if op=='+':
-		   result=int(num1)+int(num2)
-		   print("The answer is %d "%(result))
-	   elif op=='-':
-		    result=int(num1)-int(num2)
-		    print("The answer is %d "%(result))
-	   elif op=='/':
-		    result=int(num1)/int(num2)
-		    print("The answer is %d "%(result))
-	   elif op=='*':
-		    result=int(num1)*int(num2)
-		    print("The answer is %d "%(result))
-	   else:
-		    print("operation is not valid")
-   else:
-	   print("the numbers were invalid")
+    def calc(num1,num2,op):
+       if (num1.isnumeric() and num2.isnumeric())==True:
+	       if op=='+':
+		       result=int(num1)+int(num2)
+		       print("The answer is %d "%(result))
+	       elif op=='-':
+		       result=int(num1)-int(num2)
+		       print("The answer is %d "%(result))
+	       elif op=='/':
+		       result=int(num1)/int(num2)
+		       print("The answer is %d "%(result))
+	       elif op=='*':
+		       result=int(num1)*int(num2)
+		       print("The answer is %d "%(result))
+	       else:
+		        print("operation is not valid")
+       else:
+	       print("the numbers were invalid")
 
-calc(num1,num2,op)
+   
+    calc(num1,num2,op)
 
 if __name__ == '__main__':
 	main()
